849d38aa47cd952e894eac84c0296ad40efb4f9e
CTURINO1 <Christurin01@gmail.com>
2026-05-20 05:02:34 -0400

    feat: extract AWS variables into dedicated variables.tf

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name used for tagging all resources"
+  type        = string
+  default     = "project-6-hybrid"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
